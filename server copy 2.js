acf1ea578bb4e72cbd48b839b99d6065d66e95d4
mikepro4 <mikhailpro4@gmail.com>
2023-11-17 18:57:44 -0500

    another test

---
+++ server copy 2.js
@@ -0,0 +1,63 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 9999;
+const hostname = process.env.HOST || `localhost`;
+
+// when using middleware `hostname` and `port` must be provided below
+
+function ensureSecure(req, res, next) {
+    console.log('x-forwarded-proto:', req.headers["x-forwarded-proto"]); // Debug log
+    if (req.headers["x-forwarded-proto"] === "https" || req.headers["x-forwarded-proto"] === undefined) {
+        // Request was via https, so do no special handling
+        handle(req, res, `https://${req.headers.host}${req.url}` )
+    } else {
+        // Redirect to https
+        res.redirect('https://' + req.hostname + req.url);
+    }
+}
+
+
+const app = next({ dev, hostname, port })
+
+// Apply the middleware only in production
+
+const handle = app.getRequestHandler()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(ensureSecure);
+}
+
+app.prepare().then(() => {
+    createServer(async (req, res) => {
+        try {
+            // Be sure to pass `true` as the second argument to `url.parse`.
+            // This tells it to parse the query portion of the URL.
+            const parsedUrl = parse(req.url, true)
+            const { pathname, query } = parsedUrl
+
+            if (pathname === '/a') {
+                await app.render(req, res, '/a', query)
+            } else if (pathname === '/b') {
+                await app.render(req, res, '/b', query)
+            } else {
+                await handle(req, res, parsedUrl)
+            }
+
+
+        } catch (err) {
+            console.error('Error occurred handling', req.url, err)
+            res.statusCode = 500
+            res.end('internal server error')
+        }
+    })
+        .once('error', (err) => {
+            console.error(err)
+            process.exit(1)
+        })
+        .listen(port, () => {
+            console.log(`> Ready on http://${hostname}:${port}`)
+        })
+})
--- server.js
+++ server.js
@@ -1,63 +1,28 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+// server.js
+const { createServer } = require('http');
+const next = require('next');
+const absoluteUrl = require('next-absolute-url').default;
+const { parse } = require('url');
 
-const dev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 9999;
-const hostname = process.env.HOST || `localhost`;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-// when using middleware `hostname` and `port` must be provided below
+app.prepare().then(() => createServer((req, res) => {
+	const { url } = req;
+	const { protocol, host } = absoluteUrl(req);
 
-function ensureSecure(req, res, next) {
-    console.log('x-forwarded-proto:', req.headers["x-forwarded-proto"]); // Debug log
-    if (req.headers["x-forwarded-proto"] === "https" || req.headers["x-forwarded-proto"] === undefined) {
-        // Request was via https, so do no special handling
-        handle(req, res, `https://${req.headers.host}${req.url}` )
-    } else {
-        // Redirect to https
-        res.redirect('https://' + req.hostname + req.url);
-    }
-}
+	if (!dev && protocol === 'http:') {
+		res.writeHead(301, {
+			Location: `https://${host}${url}`
+		});
+		res.end();
+		return {};
+	}
 
-
-const app = next({ dev, hostname, port })
-
-// Apply the middleware only in production
-
-const handle = app.getRequestHandler()
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(ensureSecure);
-}
-
-app.prepare().then(() => {
-    createServer(async (req, res) => {
-        try {
-            // Be sure to pass `true` as the second argument to `url.parse`.
-            // This tells it to parse the query portion of the URL.
-            const parsedUrl = parse(req.url, true)
-            const { pathname, query } = parsedUrl
-
-            if (pathname === '/a') {
-                await app.render(req, res, '/a', query)
-            } else if (pathname === '/b') {
-                await app.render(req, res, '/b', query)
-            } else {
-                await handle(req, res, parsedUrl)
-            }
-
-
-        } catch (err) {
-            console.error('Error occurred handling', req.url, err)
-            res.statusCode = 500
-            res.end('internal server error')
-        }
-    })
-        .once('error', (err) => {
-            console.error(err)
-            process.exit(1)
-        })
-        .listen(port, () => {
-            console.log(`> Ready on http://${hostname}:${port}`)
-        })
-})
+	const parsedUrl = parse(url, true);
+	return handle(req, res, parsedUrl);
+}).listen(3000, (err) => {
+	if (err) throw err;
+	console.log('> Sever is running!');
+}));
